df1a1613faf3d48f7e5cbfb9fc767f9800f53961
Shinji Nakamatsu <snaka.gml@gmail.com>
2017-06-14 00:51:50 +0900

    Promise化

--- background.js
+++ background.js
@@ -2,11 +2,6 @@
   class Jobcan {
     constructor() {
       console.log("Jobcan constructor");
-      // do something
-      this.loadJobcanPage = this.loadJobcanPage.bind(this);
-      this.isLogin = this.isLogin.bind(this);
-      this.login = this.login.bind(this);
-      this.openJobcanPage = this.openJobcanPage.bind(this);
     }
 
     /*
@@ -15,37 +10,34 @@
     loadJobcanPage() {
       console.log("loadJobcanPage");
 
-      let dInner = new $.Deferred();
-
-      let promise = $.ajax({
-        type: "GET",
-        url: "https://ssl.jobcan.jp/employee/index/load-top-informations"
-      });
-      promise.done((data) => {
-        if (!this.isLogin(data)) {
-          // dInner.reject("ジョブカンにログインしてください");
-          this
-            .login()
-            .then(() => dInner.resolve())
-            .catch(() => dInner.reject("ログインできませんでした"));
-          return;
-        }
-        let html = $.parseHTML($.trim(data.tpl));
-        let statusTable = html[2];
-        let statuses = [];
-        statusTable.querySelectorAll("tr").forEach((tr) => {
-          let statusName = $(tr).children("th").text();
-          let statusCountText = $(tr).children("td").text();
-          let statusCount = Number.parseInt(statusCountText);
-          console.log(statusName + ":" + statusCount);
-          statuses.push({ title: statusName, count: statusCount });
+      return new Promise((resolve, reject) => {
+        $.ajax({
+          type: "GET",
+          url: "https://ssl.jobcan.jp/employee/index/load-top-informations"
+        }).done((data) => {
+          if (!this.isLogin(data)) {
+            // dInner.reject("ジョブカンにログインしてください");
+            this
+              .login()
+              .then(() => resolve())
+              .catch(() => reject("ログインできませんでした"));
+            return;
+          }
+          let html = $.parseHTML($.trim(data.tpl));
+          let statusTable = html[2];
+          let statuses = [];
+          statusTable.querySelectorAll("tr").forEach((tr) => {
+            let statusName = $(tr).children("th").text();
+            let statusCountText = $(tr).children("td").text();
+            let statusCount = Number.parseInt(statusCountText);
+            console.log(statusName + ":" + statusCount);
+            statuses.push({ title: statusName, count: statusCount });
+          });
+          // for test
+          // statuses = [{title: "status1", count: 1}];
+          resolve(statuses);
         });
-        // for test
-        // statuses = [{title: "status1", count: 1}];
-        dInner.resolve(statuses);
       });
-
-      return dInner.promise();
     }
 
     /*
@@ -53,30 +45,29 @@
      */
     login() {
       console.log("jobcan login");
-      var dInner = new $.Deferred();
-
       // ログイン情報を取得してログインフォームを送信
-      chrome.storage.sync.get({
-        companyId: "",
-        email: "",
-        password: ""
-      }, (items) => {
-        $.post(
-          "https://ssl.jobcan.jp/login/pc-employee",
-          {
-            client_id: items.companyId,
-            email: items.email,
-            password: items.password,
-            save_login_info: "1",
-            url: "/employee",
-            login_type: "1"
-          },
-          (data) => {
-            dInner.resolve();
-          }
-        );
+      return new Promise((resolve, reject) => {
+        chrome.storage.sync.get({
+          companyId: "",
+          email: "",
+          password: ""
+        }, (items) => {
+          $.post(
+            "https://ssl.jobcan.jp/login/pc-employee",
+            {
+              client_id: items.companyId,
+              email: items.email,
+              password: items.password,
+              save_login_info: "1",
+              url: "/employee",
+              login_type: "1"
+            },
+            (data) => {
+              resolve();
+            }
+          );
+        });
       });
-      return dInner.promise();
     }
 
     /*
@@ -169,13 +160,13 @@
     console.log("check silently");
     jobcan
       .loadJobcanPage()
-      .done((data) => {
+      .then((data) => {
         updateBadge(data);
         if (calcTotalCount(data) > 0) {
           notify(data);
         }
       })
-      .fail((message) => {
+      .catch((message) => {
         showErrorBadge();
       });
   }
@@ -186,11 +177,11 @@
   chrome.browserAction.onClicked.addListener((tab) => {
     jobcan
       .loadJobcanPage()
-      .done((data) => {
+      .then((data) => {
         updateBadge(data);
         notify(data);
       })
-      .fail((message) => {
+      .catch((message) => {
         showErrorBadge();
         notifyError(message);
       });
@@ -198,13 +189,9 @@
 
   // 定期的にチェック
   chrome.alarms.onAlarm.addListener((alarm) => {
-    jobcan
-      .login()
-      .then(checkSilently);
+    jobcan.login().then(checkSilently);
   });
-  console.log("add onAlerm listener")
-
-  chrome.alarms.create({ periodInMinutes: 30 });
+  chrome.alarms.create({ periodInMinutes: 60 });
 
   // 通知のイベント
   chrome.notifications.onButtonClicked.addListener((notificationId, buttonIndex) => {
